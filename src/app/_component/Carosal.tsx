9a0df4a334bbd5579b9d669f009be987ac0d4f7e
anand9773 <aaanand9773@gmail.com>
2024-08-13 20:06:07 +0530

    flex dotenv/config

--- src/app/_component/Carosal.tsx
+++ src/app/_component/Carosal.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const Carosal = () => {
   return (
-    <div className="hidden sm:block w-[100%] h-[130px] shadow bg-lime-50 py-1 ">
+    <div className="hidden md:block w-auto h-[130px] shadow bg-lime-50 py-1 ">
       <div className="flex gap-8 ">
         {carosalData.map((items) => (
           <div>
             <img
               src={items.image}
               alt=""
-              className="rounded-full w-[120px] h-[120px]"
+              className="rounded-full  md:w-[86px] md:h-[86px] lg:w-[120px] lg:h-[120px]"
             />
           </div>
         ))}
